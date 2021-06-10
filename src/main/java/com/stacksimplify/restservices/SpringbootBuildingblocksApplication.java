dae5fece3646c204b6aa5f0e3e8ceafe20c207c2
maniyabista <maniyabista@maniyas-mbp.attlocal.net>
2021-06-10 15:08:12 -0500

    Second commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -3,6 +3,7 @@ package com.stacksimplify.restservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //new line
+//new line2
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
